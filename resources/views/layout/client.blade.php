e604d03e0a7ac47fcb5a839f623c79d8d988e26d
anhnt-theanh26 <nguyentheanh260204@gmail.com>
2025-06-08 21:20:48 +0700

    night 08/06

--- resources/views/layout/client.blade.php
+++ resources/views/layout/client.blade.php
@@ -14,12 +14,14 @@
 <body class="bg-light">
     @include('sweetalert::alert'){{-- libary alert --}}
     <!-- header  -->
-    <header class="position-fixed w-100" style="z-index: 10; top: 0;">
+    {{-- <header class="position-fixed w-100" style="z-index: 10; top: 0;"> --}}
+    <header>
         @include('client.element.header')
     </header>
     <!--hết header  -->
 
-    <main style="margin-top: 150px">
+    {{-- <main style="margin-top: 150px"> --}}
+    <main>
         @yield('content')
     </main>
 
